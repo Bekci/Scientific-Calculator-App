cb4176ba7322afced8344a1b9053ceb4cb3ccb87
Burak <bekciburak97@gmail.com>
2017-10-09 23:19:55 +0300

    Function for buttons added. Most of the buttons write to edit text but to the end of it

--- app/src/main/java/com/example/android/scientificcalculator/ButtonFunctions.java
+++ app/src/main/java/com/example/android/scientificcalculator/ButtonFunctions.java
@@ -2,17 +2,43 @@ package com.example.android.scientificcalculator;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import static android.R.attr.tag;
 import static com.example.android.scientificcalculator.R.layout.activity_main;
+import static java.security.AccessController.getContext;
 
 /**
  * Created by Burak on 9.10.2017.
  */
 
 public class ButtonFunctions {
-    private static int activity_main;
 
     public static String getText(Button button){
-        return button.getText().toString();
+        String toReturn = "";
+        String tag = button.getTag().toString();
+        if(tag.endsWith("opr") || tag.endsWith("num") || tag.endsWith("point")){
+            toReturn = button.getText().toString();
+        }
+        else if(tag.endsWith("root")){
+            toReturn = "sqrt(";
+        }
+        else if(tag.endsWith("oprp")){
+            toReturn = button.getText().toString() + "(";
+        }
+        else if(tag.endsWith("pow")){
+            toReturn = "^(";
+        }
+        else if(tag.endsWith("fac")){
+            toReturn = "!";
+        }
+        else if(tag.endsWith("exp")){
+            toReturn = "e^(";
+        }
+        else{
+            toReturn = "Not assigned yet";
+        }
+
+        return toReturn;
     }
 }
--- app/src/main/java/com/example/android/scientificcalculator/MainActivity.java
+++ app/src/main/java/com/example/android/scientificcalculator/MainActivity.java
@@ -4,17 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Replace edit text with textview
+    private EditText expression;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        expression = (EditText) findViewById(R.id.Expression);
     }
-    private void buttonClicked(View view){
+    public void buttonClicked(View view){
         int  x = view.getId();
         Button button = (Button) findViewById(x);
-        ButtonFunctions.getText(button);
+        String butText = ButtonFunctions.getText(button);
+        Toast.makeText(getBaseContext(),butText,Toast.LENGTH_SHORT ).show();
+        String expressionStr = expression.getText().toString();
+        expressionStr += butText;
+        expression.setText(expressionStr);
+        //Place strings in cursor's place.
+        //At first set cursor place to end of text.
+
     }
 }
